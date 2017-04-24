a05b096cc7c6e16e773f8a84cb965299c2b20e16
student <student@K0602>
2017-04-24 11:39:34 +0300

    Addet AddFunk

---
+++ Calculator2/AddOperation.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator2
+{
+    class AddOperation
+    {
+        public double addFunk(double a, double b)
+        {           
+            return a + b;
+        }
+    }
+}
