443a8156ae1c476d77f19102a3e81394a690419d
6pAT Eu <6pAT.ua@gmail.com>
2017-04-24 11:39:36 +0300

    added class DevideOperation

---
+++ Calculator2/DivideOperation.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator2
+{
+    class DivideOperation
+    {
+        public int Divide(int x, int y)
+        {
+                return x / y;
+        }
+    }
+}
