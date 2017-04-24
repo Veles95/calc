20825777f87d9887f66fade7bba564aed4b7c8bd
IrynaCha <kurnosova.iryna@gmail.com>
2017-04-24 11:39:22 +0300

    added class MultiplyOperation

---
+++ Calculator2/MultiplyOperation.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator2
+{    
+    class MultiplyOperation
+    {
+        double a, b;
+        public double Multiply()
+        {
+            a = 1.4;
+            b = 1.5;
+            double c = a * b;
+            return c;
+        }
+    }
+}
